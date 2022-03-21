dd21ab4c889bd571e6779fd6da7b2a896ece2210
Billions Joel <joelatugonza@gmail.com>
2022-03-21 18:38:31 +0300

    Edit queries to varchar

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals(
   id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name TEXT NOT NULL,
+  name VARCHAR(255) NOT NULL,
   date_of_birth DATE NOT NULL,
   escape_attempts INT NOT NULL,
   neutered BOOLEAN NOT NULL,

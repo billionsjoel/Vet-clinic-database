ce83d924c3abacfcdf819aab2e74eb70ae3e0947
Billions Joel <joelatugonza@gmail.com>
2022-03-21 17:25:08 +0300

    Run query for neutered animals with less than 3 escapes

--- queries.sql
+++ queries.sql
@@ -2,3 +2,4 @@
 
 SELECT DISTINCT name FROM animals WHERE name LIKE '%mon';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT * from animals WHERE neutered = true AND escape_attempts < 3;

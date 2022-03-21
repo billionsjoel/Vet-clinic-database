f7b45937621ed82311aaedcd6e395f50d2942e5b
Billions Joel <joelatugonza@gmail.com>
2022-03-21 17:39:13 +0300

    Run query for all neutered animals

--- queries.sql
+++ queries.sql
@@ -4,3 +4,5 @@ SELECT DISTINCT name FROM animals WHERE name LIKE '%mon';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
 SELECT * from animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name ='Pikachu';
+SELECT name,escape_attempts FROM animals WHERE weight_kg > 10.5;
+select * from animals where neutered=true;

e47e31e1d6ca8b7ffcebb350c74bd2119e8e9d5c
Billions Joel <joelatugonza@gmail.com>
2022-03-21 17:32:08 +0300

    Run for dob either or

--- queries.sql
+++ queries.sql
@@ -3,3 +3,4 @@
 SELECT DISTINCT name FROM animals WHERE name LIKE '%mon';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
 SELECT * from animals WHERE neutered = true AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name ='Pikachu';

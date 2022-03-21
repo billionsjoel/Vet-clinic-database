1e23044f3192e7661134ad29c3bb82840d8c1add
Billions Joel <joelatugonza@gmail.com>
2022-03-21 16:23:55 +0300

    Add animal db and recreate schema

--- schema.sql
+++ schema.sql
@@ -3,10 +3,10 @@
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals(
-  id INT PRIMARY KEY NOT NULL,
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name TEXT NOT NULL,
   date_of_birth DATE NOT NULL,
-  escape_attempts INT,
-  nuetered BOOL,
-  weight_kg DECIMAL
+  escape_attempts INT NOT NULL,
+  neutered BOOLEAN NOT NULL,
+  weight_kg DECIMAL NOT NULL
 );

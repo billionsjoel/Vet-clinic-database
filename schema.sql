f7b2babd351bd414e64dfa996eb5452b404c86d6
Billions Joel <joelatugonza@gmail.com>
2022-03-21 15:51:44 +0300

    Add animal db and create schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE DATABASE vet_clinic;
+
+CREATE TABLE animals(
+  id INT PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  date_of_birth DATE NOT NULL,
+  escape_attempts INT,
+  nuetered BOOL,
+  weight_kg DECIMAL
 );

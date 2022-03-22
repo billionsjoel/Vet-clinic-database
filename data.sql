16d637970a14c9943a348562bc7bbc008624c04e
Billions Joel <joelatugonza@gmail.com>
2022-03-23 00:57:57 +0300

    Insert data in owners table

--- data.sql
+++ data.sql
@@ -29,3 +29,10 @@ VALUES
   ('Boarmon', '2005-06-07', 7, true, 20),
   ('Blossom', '1998-10-13', 3, true, 17),
   ('Ditto', '2022-05-14', 4, true, 22);
+
+insert into owners(full_name, age) values('sam smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);

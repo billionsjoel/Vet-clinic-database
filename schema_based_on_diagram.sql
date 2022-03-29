3c20c8257511bfba00acf040bbb7b81ac1ac7ac0
Billions Joel <joelatugonza@gmail.com>
2022-03-29 15:38:12 +0300

    Create table invoice items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,7 +12,14 @@ CREATE TABLE treatment(
   name VARCHAR(100) NOT NULL
 );
 
-
+CREATE TABLE invoice_items(
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  unit_price DECIMAL NOT NULL,
+  quantity INT NOT NULL,
+  total_price DECIMAL NOT NULL,
+  invoice_id INT NOT NULL,
+  treatment_id INT NOT NULL
+);
 
 
 
